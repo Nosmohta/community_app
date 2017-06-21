62b8e92f40ef4e446d32141a20b92688e32f6598
Richard Forrester <richard.forrester@gmail.com>
2017-06-21 18:18:19 +0000

    fixed final conflict in root reducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,10 +1,10 @@
 import {combineReducers} from 'redux';
-import users from './userReducers';
+import user from './userReducers';
 import topics from './topicReducers';
 
 const rootReducer = combineReducers({
   // short hand property names
-  users: users,
+  user: user,
   topics: topics
 
 })
