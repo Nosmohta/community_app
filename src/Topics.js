f721a9cc6676cbd60fd172e43945110840f68acb
Richard Forrester <richard.forrester@gmail.com>
2017-06-21 17:54:35 +0000

    working on conversation form in Conversation component

--- src/Topics.js
+++ src/Topics.js
@@ -2,31 +2,29 @@ import {connect} from 'react-redux';
 import React, {Component} from 'react';
 import 'bulma/css/bulma.css'
 import './Topic.css';
+import Conversation from './Conversation.js';
 import {Columns} from 'bulma-components'
 
 class Topics extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-                   topics: []
-                 };
-
-    }
+    this.state = { topics: [] };
+  }
 
 
-componentWillReceiveProps(nextProps) {
-  this.setState({topics: nextProps.topics});
-}
+  componentWillReceiveProps(nextProps) {
+    this.setState({topics: nextProps.topics});
+  }
 
-render() {
-  const topics = []
-  Object.keys(this.props.topics).map((key) => {
-     topics.push(this.props.topics[key])
-  })
-
-
+  render() {
+    const topics = []
+    Object.keys(this.props.topics).map((key) => {
+       topics.push(this.props.topics[key])
+    })
     return (
+
             <div className="container">
+            <Conversation/>
             {topics.map((a) => {
               return (
             <div className="column topics-canvas">
@@ -59,9 +57,7 @@ render() {
 }
 
 function mapStateToProps(state, ownProps) {
-console.log('from Topics component ', state.topics)
-  return {
-    topics: state.topics
-  };
+  console.log('from Topics component ', state.topics)
+    return { topics: state.topics };
 }
 export default connect(mapStateToProps)(Topics);
