db9bb24f117315cf6a963760425da6931bfe4e15
Andrew Thomson <Thomson.w.andrew@gmail.com>
2017-06-26 15:32:44 +0000

    conversation.js added back to file

---
+++ src/Conversation.js
@@ -0,0 +1,29 @@
+import {connect} from 'react-redux';
+import React, {Component} from 'react';
+import 'bulma/css/bulma.css'
+import './Conversation.css';
+import {Columns} from 'bulma-components'
+import UploadPhoto from './Conversation/UploadPhoto.js'
+import Description from './Conversation/Description.js'
+import Subject from './Conversation/Subject.js'
+
+class Conversation extends Component {
+
+
+
+  render() {
+
+
+    return (
+      <div className="conversation-container">
+        <UploadPhoto/>
+        <Description/>
+        <Subject/>
+      </div>
+    )
+
+    
+  }
+}
+
+export default Conversation;
